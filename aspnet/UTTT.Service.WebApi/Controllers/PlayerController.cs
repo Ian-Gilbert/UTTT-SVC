a796720597b6be66003b09fce212a7b0038911d0
Ian Gilbert <ian.d.gilbert@gmail.com>
2020-12-06 18:21:53 -0800

    add DataContext classlib and update project references

--- aspnet/UTTT.Service.WebApi/Controllers/PlayerController.cs
+++ aspnet/UTTT.Service.WebApi/Controllers/PlayerController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace UTTT.Service.WebApi.Controllers
 {
     [ApiController]
+    [Route("rest/player")]
     public class PlayerController : ControllerBase
     {
 
