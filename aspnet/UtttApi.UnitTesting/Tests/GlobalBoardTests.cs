f09ceede15cc4ed441f97084d4bbfc1dfad9c0e6
Ian Gilbert <ian.d.gilbert@gmail.com>
2021-01-24 15:15:06 -0800

    add global board tests

--- aspnet/UtttApi.UnitTesting/Tests/GlobalBoardTests.cs
+++ aspnet/UtttApi.UnitTesting/Tests/GlobalBoardTests.cs
@@ -1,3 +1,4 @@
+using UtttApi.ObjectModel.Enums;
 using UtttApi.ObjectModel.Models;
 using Xunit;
 
@@ -5,10 +6,17 @@ namespace UtttApi.UnitTesting.Tests
 {
     public class GlobalBoardTests
     {
+        private readonly Move move;
+
         public GlobalBoard gb { get; set; }
 
         public GlobalBoardTests()
         {
+            var player = MarkType.PLAYER1;
+            var lbIndex = 0;
+            var markIndex = 0;
+            move = new Move() { Mark = player, LbIndex = lbIndex, MarkIndex = markIndex };
+
             gb = new GlobalBoard();
         }
 
@@ -22,7 +30,109 @@ namespace UtttApi.UnitTesting.Tests
         [Fact]
         public void IsValidMove_ReturnsFalse_WhenLbIsNotInFocus()
         {
+            gb.LocalBoards[move.LbIndex].Focus = false;
 
+            var result = gb.IsValidMove(move);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidMove_ReturnsFalse_WhenMarkIsNotEmpty()
+        {
+            gb.LocalBoards[move.LbIndex].Board[move.MarkIndex] = MarkType.DRAW;
+
+            var result = gb.IsValidMove(move);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidMove_ReturnsTrue_WhenMoveIsValid()
+        {
+            var result = gb.IsValidMove(move);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void MakeMove_MarksLocalBoard()
+        {
+            gb.MakeMove(move);
+
+            Assert.Equal(move.Mark, gb.LocalBoards[move.LbIndex].Board[move.MarkIndex]);
+        }
+
+        [Fact]
+        public void MakeMove_MarksGlobalBoard_WhenLocalBoardHasTicTacToe()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                gb.LocalBoards[move.LbIndex].Board[i] = move.Mark;
+            }
+
+            gb.MakeMove(move);
+
+            Assert.Equal(move.Mark, gb.Board[move.LbIndex]);
+        }
+
+        [Fact]
+        public void MakeMove_MarksGlobalBoard_WhenLocalBoardDraws()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                gb.LocalBoards[move.LbIndex].Board[i] = MarkType.DRAW;
+            }
+
+            gb.MakeMove(move);
+
+            Assert.Equal(MarkType.DRAW, gb.Board[move.LbIndex]);
+        }
+
+        [Fact]
+        public void UpdateFocus_ShouldSetEverythingToFalse_WhenGameIsFinished()
+        {
+            gb.UpdateFocus(move, GameStatus.DRAW);
+
+            foreach (var lb in gb.LocalBoards)
+            {
+                Assert.False(lb.Focus);
+                Assert.False(lb.Playable);
+            }
+        }
+
+        [Fact]
+        public void UpdateFocus_ShouldSetOnlyNextLbInFocus_WhenNextLbIsPlayableAndGameIsInProgress()
+        {
+            var nextLb = gb.LocalBoards[move.MarkIndex];
+
+            gb.UpdateFocus(move, GameStatus.IN_PROGRESS);
+
+            foreach (var lb in gb.LocalBoards)
+            {
+                if (lb == nextLb)
+                {
+                    Assert.True(lb.Focus);
+                }
+                else
+                {
+                    Assert.False(lb.Focus);
+                }
+            }
+        }
+
+        [Fact]
+        public void UpdateFocus_ShouldSetAllPlayableLbsInFocus_WhenNextLbIsNotPlayableAndGameIsInProgress()
+        {
+            var nextLb = gb.LocalBoards[move.MarkIndex];
+            nextLb.Playable = false;
+
+            gb.UpdateFocus(move, GameStatus.IN_PROGRESS);
+
+            foreach (var lb in gb.LocalBoards)
+            {
+                Assert.Equal(lb.Playable, lb.Focus);
+            }
         }
     }
 }
