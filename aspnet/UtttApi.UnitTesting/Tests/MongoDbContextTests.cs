8cd6a2474b96609c7fba8b67432be917bfa5dcdf
Ian Gilbert <ian.d.gilbert@gmail.com>
2021-01-23 17:40:13 -0800

    add MongoDbContext tests

--- aspnet/UtttApi.UnitTesting/Tests/MongoDbContextTests.cs
+++ aspnet/UtttApi.UnitTesting/Tests/MongoDbContextTests.cs
@@ -1,21 +1,39 @@
+using UtttApi.DataService;
+using UtttApi.DataService.Settings;
+using UtttApi.ObjectModel.Models;
 using Xunit;
 
 namespace UtttApi.UnitTesting.Tests
 {
     public class MongoDbContextTests
     {
+        private readonly MongoDbContext context;
         public MongoDbContextTests()
         {
+            var settings = new MongoDbSettings()
+            {
+                ConnectionString = "mongodb://test123",
+                DatabaseName = "Test"
+            };
+            context = new MongoDbContext(settings);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void GetCollection_ReturnsNull_WhenNameIsNullOrEmpty(string collectionName)
+        {
+            var collection = context.GetCollection<UtttObject>(collectionName);
+            Assert.Null(collection);
         }
 
         [Fact]
-        public void TestName()
+        public void GetCollection_ReturnsCollection_WhenNameIsNotEmpty()
         {
-            //Given
-
-            //When
-
-            //Then
+            var collectionName = "testCollection";
+            var collection = context.GetCollection<UtttObject>(collectionName);
+            Assert.NotNull(collection);
+            Assert.Equal(collectionName, collection.CollectionNamespace.CollectionName);
         }
     }
 }
